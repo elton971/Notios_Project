01d6df0ccfec8dc900a31b240ea9ca16885a35fa
Elton Cavele <82831720+elton971@users.noreply.github.com>
2022-07-05 15:39:29 +0200

    add

--- src/Components/AddPage.tsx
+++ src/Components/AddPage.tsx
@@ -1,24 +1,30 @@
 import style from '../Styles/AddPage.module.css'
-
 import addInside from "../Assents/addInside.svg"
-
 import { useState } from 'react'
 import { NewPage } from './NewPage'
+import { Navbar } from './Navbar.'
 
 export const AddPage =()=>{
     
-    const [show, setShow] = useState(false); //para visualizar a pagina
+    const [newPage, setnewPage] = useState<any>([]); //para visualizar a pagina
+    function addPagesTaskes(){
+        console.log("ola")
+        setnewPage([...newPage, <NewPage/>])
+        
+    }
     return (
         <div className={style.addPageContainer}>
             <div className={style.addPageContent}>
-                {
-                    show ? <NewPage /> : null
-                }
+               {
+                    newPage.map((navbar: any)=>{
+                        return navbar;
+                    })
+               }
                 <div className={style.addPageContentAddButton}>
 
                     <button className={style.settingsContentButton}
                     onClick={()=>{
-                      setShow(!show)
+                        addPagesTaskes()
                     }}
                     >
                         <img src={addInside} alt="addInside"  />
